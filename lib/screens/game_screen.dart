5eed122108b63ffb06acfd41d668423f57e36f89
chutiw02 <66011212163@msu.ac.th>
2026-07-18 23:15:57 +0700

    status, restart

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../utils/player.dart';
+import '../utils/game_result.dart';
 import '../providers/game_provider.dart';
 import '../widgets/board_widget.dart';
+import '../utils/player.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({super.key});
@@ -12,31 +13,69 @@ class GameScreen extends StatelessWidget {
     final provider = context.watch<GameProvider>();
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Game"),
-      ),
+      appBar: AppBar(title: const Text("Game")),
+
       body: Padding(
         padding: const EdgeInsets.all(16),
+
         child: Column(
           children: [
-            Text(
-              "Current Player : ${provider.currentPlayer.symbol}",
-              style: const TextStyle(
-                fontSize: 22,
-              ),
-            ),
+            _buildStatus(provider),
 
             const SizedBox(height: 20),
 
             Expanded(
               child: BoardWidget(
                 board: provider.board,
-                onCellTap: provider.makeMove,
+
+                onCellTap: (row, col) {
+                  provider.makeMove(row, col);
+                },
               ),
+            ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                provider.initializeBoard(provider.boardSize);
+              },
+
+              child: const Text("Restart"),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildStatus(GameProvider provider) {
+    if (provider.isGameOver) {
+      return Text(
+        _resultText(provider),
+        style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      );
+    }
+
+    return Text(
+      "Turn : ${provider.currentPlayer.symbol}",
+      style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    );
+  }
+
+  String _resultText(GameProvider provider) {
+    switch (provider.gameResult) {
+      case GameResult.xWin:
+        return "X Winner 🎉";
+
+      case GameResult.oWin:
+        return "O Winner 🎉";
+
+      case GameResult.draw:
+        return "Draw";
+
+      default:
+        return "";
+    }
   }
 }
